ca399dd4349f8ceca9410b85373d55d914ec2553
Thangnv <katothang@gmail.com>
2022-06-12 10:21:18 +0700

    update

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import os
 from coin import *
 from tele import *
 
-def main():
+def getPriceCoin():
     last_textchat = (None, None)
     while True:
-        text, chat = get_last_chat_id_and_text(get_updates())
-        if (text, chat) != last_textchat:
-            send_message(get_biance_price(text), chat)
-            last_textchat = (text, chat)
-        time.sleep(1)
+        try:
+            text, chat = get_last_chat_id_and_text(get_updates())
+            if (text, chat) != last_textchat:
+                send_message(get_biance_price(text), chat)
+                last_textchat = (text, chat)
+            time.sleep(1)
+        except:
+            print("abc")
+def main():
+    getPriceCoin()
 
 if __name__ == '__main__':
     main()
-
