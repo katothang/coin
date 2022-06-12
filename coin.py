c98d51d4db9b28396995f343303487d4b999bc51
Thangnv <katothang@gmail.com>
2022-06-12 13:15:37 +0700

    update auto

--- coin.py
+++ coin.py
@@ -5,14 +5,35 @@ TOKEN = "5399039858:AAGn6Ak5kQfB9QOKGPm63QxFbknF_NXGIHE"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 API_PRICE="https://api.binance.com/api/v1/ticker/price?symbol={}"
 API_24H="https://api.binance.com/api/v1/ticker/24hr?symbol={}"
+LIST_COIN=["SHIB", "EOS", "ANC", "EXP","MIR","XEC", "JASMY", "PEOPLE"]
+
 def get_biance_price(coinName):
+    if coinName.upper() == 'LIST':
+        return get_list_coin_price(False)
     coinName = coinName.upper()+'USDT'.upper()
     url = requests.get(API_24H.format(coinName))
     data = url.json()
     if "code" in data:
         return "Mã coin không tồn tại.!"
-    return "Giá hiện tại {} Phần trăm thay đổi {}% ".format(data["lastPrice"], data["priceChangePercent"])
+    return "({})Giá hiện tại {} Phần trăm thay đổi {}% ".format(coinName.upper(),data["lastPrice"], data["priceChangePercent"])
 
+def get_biance_price_auto(coinName):
+    coinName = coinName.upper()+'USDT'.upper()
+    url = requests.get(API_24H.format(coinName))
+    data = url.json()
+    if "code" in data:
+        return ""
+    if float(data["priceChangePercent"]) > 10 or float(data["priceChangePercent"]) < -10:
+        return "({})Giá hiện tại {} Phần trăm thay đổi {}% ".format(coinName.upper(),data["lastPrice"], data["priceChangePercent"])
+    return ""
 
+def get_list_coin_price(isAuto):
+    result = ""
+    for coinName in LIST_COIN:
+        if isAuto:
+            result+= get_biance_price_auto(coinName) + "\n"
+        else:
+            result+= get_biance_price(coinName) + "\n"
+    return result
 
 
--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import sys
 import os
 from coin import *
 from tele import *
+from threading import Thread
 
 def getPriceCoin():
     last_textchat = (None, None)
     while True:
         try:
             text, chat = get_last_chat_id_and_text(get_updates())
-            if (text, chat) != last_textchat:
+            if (text, chat) != last_textchat and len(text) < 10:
                 send_message(get_biance_price(text), chat)
                 last_textchat = (text, chat)
-            time.sleep(1)
+            time.sleep(0.5)
         except:
             print("abc")
+def getPriceCoinAuto():
+    while True:
+        result = get_list_coin_price(True)
+        if(result != ""):
+            send_message(result, "1625452249")
+        time.sleep(600)
+
 def main():
+    thread = Thread(target = getPriceCoinAuto)
+    thread.start()
+    thread.join()
+
     getPriceCoin()
 
 if __name__ == '__main__':
